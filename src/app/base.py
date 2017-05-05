d3409f75f78c3fcb02d3d501eff538b3f779e42c
Jma353 <jma353@cornell.edu>
2017-05-05 17:49:26 -0400

    Add base

---
+++ src/app/base.py
@@ -0,0 +1,10 @@
+from marshmallow_sqlalchemy import ModelSchema
+from . import db
+
+class Base(db.Model):
+  """
+  Base database model
+  """
+  __abstract__ = True
+  created_at = db.Column(db.DateTime, default = db.func.current_timestamp())
+  updated_at = db.Column(db.DateTime, default = db.func.current_timestamp())
